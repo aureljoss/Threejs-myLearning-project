77cf484a731a08e45ec34ab1e10c19ff39580093
Aurelie <josserand.n.aurelie@gmail.com>
2024-11-10 19:08:13 -0500

    Adds a mesh ,camera, scene, and renderer to the scene.

--- src/script.js
+++ src/script.js
@@ -1,18 +1,40 @@
 import * as THREE from 'three'
 
-//scene
-const scene = new THREE.Scene()
-
 //Mesh
 const mesh=new THREE.Mesh(
-    THREE.BufferGeometry(1,1,1),
-    THREE.MeshBasicMaterial({color: 'red'}) )
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 'red'}) )
+mesh.position.set(1,0,0)
+mesh.rotation.x=Math.PI*0.25
 
+//Sets up basic sizes
 const sizes={
     width: 800, 
     height:600
 }
 
-const camera = new THREE.PerspectiveCamera(35, sizes.width/sizes.height)
+//Camera
+const camera = new THREE.PerspectiveCamera(70, sizes.width/sizes.height)
+camera.position.z=3
 
-scene.add(mesh)
+//Look at this!
+camera.lookAt(mesh.position)
+
+//Canvas
+const canvas= document.querySelector('canvas.webgl')
+
+//Scene
+const scene = new THREE.Scene()
+scene.add(mesh, camera)
+
+//Axes helper
+const axesHelper=new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+//Renderer
+const renderer= new THREE.WebGLRenderer({
+    canvas:canvas
+})
+
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
