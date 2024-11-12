213ad2a938132306020905da10b51b4e0c842943
Aurelie <josserand.n.aurelie@gmail.com>
2024-11-11 19:15:02 -0500

    Testing out groups

--- src/script.js
+++ src/script.js
@@ -1,40 +1,55 @@
-import * as THREE from 'three'
+import * as THREE from "three";
+import { Object3D } from "three";
 
 //Mesh
-const mesh=new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1),
-    new THREE.MeshBasicMaterial({color: 'red'}) )
-mesh.position.set(1,0,0)
-mesh.rotation.x=Math.PI*0.25
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "red" })
+);
+mesh.position.set(1, 0, 0);
+mesh.rotation.x = Math.PI * 0.25;
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "blue" })
+);
+cube1.position.set(-1, 0, 0);
 
 //Sets up basic sizes
-const sizes={
-    width: 800, 
-    height:600
-}
+const sizes = {
+  width: 800,
+  height: 600,
+};
 
 //Camera
-const camera = new THREE.PerspectiveCamera(70, sizes.width/sizes.height)
-camera.position.z=3
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+
+//Group
+const groupCubes = new THREE.Group();
+groupCubes.add(cube1, mesh);
+groupCubes.position.y = 1;
 
 //Look at this!
-camera.lookAt(mesh.position)
+camera.lookAt(cube1.position);
 
 //Canvas
-const canvas= document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 //Scene
-const scene = new THREE.Scene()
-scene.add(mesh, camera)
+const scene = new THREE.Scene();
+scene.add(groupCubes, camera);
 
 //Axes helper
-const axesHelper=new THREE.AxesHelper(2)
-scene.add(axesHelper)
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
 
 //Renderer
-const renderer= new THREE.WebGLRenderer({
-    canvas:canvas
-})
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
 
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+console.log(Object3D);
