5a288ad3107c9568a0e64086bac0a6f283362d52
Aurelie <josserand.n.aurelie@gmail.com>
2024-11-23 15:25:18 -0500

    Handles viewport resize with addition of an event listener

--- src/script.js
+++ src/script.js
@@ -23,6 +23,24 @@ const sizes = {
   height: window.innerHeight,
 };
 
+
+//Handling of screen resizing
+window.addEventListener('resize', () =>
+  {
+      // Update sizes
+      sizes.width = window.innerWidth
+      sizes.height = window.innerHeight
+  
+      // Update camera
+      camera.aspect = sizes.width / sizes.height
+      camera.updateProjectionMatrix()
+  
+      // Update renderer
+      renderer.setSize(sizes.width, sizes.height)
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  })
+
+
 //Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
