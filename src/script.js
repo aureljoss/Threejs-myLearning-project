0c5493377e054140401fe24d8973c83c4b226d08
Aurelie <josserand.n.aurelie@gmail.com>
2024-11-24 19:40:12 -0500

    Installs lil-gui. Add gui debug options including onFinishChange button

--- src/script.js
+++ src/script.js
@@ -1,8 +1,9 @@
 import * as THREE from "three";
 import { Object3D } from "three";
-import gsap from 'gsap';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import gsap from "gsap";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { BufferAttribute } from "three";
+import GUI from "lil-gui";
 
 //Mesh
 const mesh = new THREE.Mesh(
@@ -13,22 +14,25 @@ mesh.position.set(1, 0, 0);
 mesh.rotation.x = Math.PI * 0.25;
 
 const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1,2,2,2),
-  new THREE.MeshBasicMaterial({ color: "blue", wireframe:true})
+  new THREE.BoxGeometry(1, 1, 1, 2, 2, 2),
+  new THREE.MeshBasicMaterial({ color: "blue", wireframe: true })
 );
 cube1.position.set(-1, 0, 0);
 
 //Buffer Geometry
-const geometry= new THREE.BufferGeometry()
-const count = 50
-const positionsArray= new Float32Array(count *9)
-for (let i=0; i<count *9; i++){
-  positionsArray[i]=(Math.random()-0.5)*4
+const bufferGeometry = new THREE.BufferGeometry();
+const count = 50;
+const positionsArray = new Float32Array(count * 9);
+for (let i = 0; i < count * 9; i++) {
+  positionsArray[i] = (Math.random() - 0.5) * 4;
 }
-const positionsAttribute= new THREE.BufferAttribute(positionsArray, 3)
-geometry.setAttribute('position', positionsAttribute)
-const materialGeometry= new THREE.MeshBasicMaterial({color: 'green', wireframe:true})
-const meshBuffer= new THREE.Mesh(geometry, materialGeometry)
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+bufferGeometry.setAttribute("position", positionsAttribute);
+const materialBufferGeometry = new THREE.MeshBasicMaterial({
+  color: "green",
+  wireframe: true,
+});
+const meshBuffer = new THREE.Mesh(bufferGeometry, materialBufferGeometry);
 
 //Sets up basic sizes
 const sizes = {
@@ -36,23 +40,20 @@ const sizes = {
   height: window.innerHeight,
 };
 
-
 //Handling of screen resizing
-window.addEventListener('resize', () =>
-  {
-      // Update sizes
-      sizes.width = window.innerWidth
-      sizes.height = window.innerHeight
-  
-      // Update camera
-      camera.aspect = sizes.width / sizes.height
-      camera.updateProjectionMatrix()
-  
-      // Update renderer
-      renderer.setSize(sizes.width, sizes.height)
-      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-  })
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 //Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
@@ -62,8 +63,8 @@ camera.position.z = 3;
 const canvas = document.querySelector("canvas.webgl");
 
 //Controls
-const controls= new OrbitControls(camera, canvas)
-controls.enableDamping=true;
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 //Group
 const groupCubes = new THREE.Group();
@@ -81,6 +82,39 @@ scene.add(groupCubes, camera, meshBuffer);
 const axesHelper = new THREE.AxesHelper(2);
 scene.add(axesHelper);
 
+//Debug
+const gui = new GUI();
+
+const meshTweaks= gui.addFolder('Mesh Tweaks')
+meshTweaks.add(mesh.position, "y").min(-10).max(10).step(0.01).name("Mesh Elevation");
+meshTweaks.add(mesh, "visible").name("is Mesh Visible?");
+meshTweaks.addColor(materialBufferGeometry, "color").name("Buffer Geometry Color");
+
+const cube1Tweaks=gui.addFolder('Cube1 Tweaks')
+const debugObject = {};
+debugObject.spin = () => {
+  gsap.to(cube1.rotation, { duration: 1, y: cube1.rotation.y + Math.PI * 2 });
+};
+cube1Tweaks.add(debugObject, "spin").name("Spin Red Cube");
+
+debugObject.subdivision = 2
+gui
+  .add(debugObject, "subdivision")
+  .min(1)
+  .max(20)
+  .step(1)
+  .onFinishChange(() => {
+    cube1.geometry.dispose();
+    cube1.geometry = new THREE.BoxGeometry(
+      1,
+      1,
+      1,
+      debugObject.subdivision,
+      debugObject.subdivision,
+      debugObject.subdivision
+    );
+  });
+
 //Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
@@ -88,19 +122,18 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setSize(sizes.width, sizes.height);
 
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-    //Update objects
-    groupCubes.rotation.y=Math.sin(elapsedTime)
-    groupCubes.rotation.x=Math.cos(elapsedTime)
-    //Update controls
-    controls.update()
-    //render
-    renderer.render(scene, camera);
-    //Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-tick()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  //Update objects
+  groupCubes.rotation.y = Math.sin(elapsedTime);
+  groupCubes.rotation.x = Math.cos(elapsedTime);
+  //Update controls
+  controls.update();
+  //render
+  renderer.render(scene, camera);
+  //Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+tick();
